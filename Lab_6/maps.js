4be28d8188dac636e7ad6a1a28ccc2bf360c765a
mmcel <mm69@yahoo.com>
2025-02-23 23:35:07 -0600

    created filed for lab 6

--- Lab_6/maps.js
+++ Lab_6/maps.js
@@ -2,6 +2,7 @@ let metroAreaCenterCoordinates = [44.96, -93.2]  // Array of latitude and longit
 let zoomLevel = 9   // 1 = whole world, 10 = large city, 20 = city blocks
 
 // Create the map 
+// L.map() is a Leaflet function that creates a map object
 let map = L.map('college-map').setView(metroAreaCenterCoordinates, zoomLevel)
 
 // Add the tile layer - roads, streets etc. Without this, nothing to see 
@@ -11,20 +12,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 // Add some markers for Minneapolis College and Saint Paul College
+
+// Marker for Minneapolis College
 let mctcCoordinates = [44.9724, -93.2844]
 let mctcMarker = L.marker(mctcCoordinates)
+    // .binfPopup allows you to click on the marker and see a popup
     .bindPopup('Minnepolis College<br><a href="https://minneapolis.edu">Website</a>')
     .addTo(map)
 
+// Marker for Saint Paul College
 let stPaulCoordinates = [44.94839, -93.1099]
 let stpMarker = L.marker(stPaulCoordinates)
     .bindPopup('Saint Paul College<br><a href="https://saintpaul.edu">Website</a>')
     .addTo(map)
 
+// Marker for Normandale College
+let normandaleCoordinates = [44.8297, -93.3312]
+let normandaleMarker = L.marker(normandaleCoordinates)
+    .bindPopup('Normandale College<br><a href="https://normandale.edu">Website</a>')
+    .addTo(map)
+
 
 // Add a circle approximately around the Twin Cities metro 
 let metroAreaCircle = L.circle(metroAreaCenterCoordinates, {
-        color: 'green',    // use any CSS color name
+        // 
+        color: 'blue',    // use any CSS color name
         radius: 30000,
         fillOpacity: 0.1
     })
