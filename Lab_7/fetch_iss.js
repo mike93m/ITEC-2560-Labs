bfe8c6a572ed22c42a6f43a145070e1ffcaad141
mmcel <mm69@yahoo.com>
2025-03-03 15:33:44 -0600

    Added comments to fetch_iss

--- Lab_7/fetch_iss.js
+++ Lab_7/fetch_iss.js
@@ -1,40 +1,51 @@
+// Variable for the URL of the ISS API
 let url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
+// Variables for the elements in the HTML
 let issLat = document.querySelector('#iss-lat')
 let issLong = document.querySelector('#iss-long')
 let timeIssLocationFetched = document.querySelector('#timeDate')
 
+// Create a map centered in the middle of the world
 let map = L.map('iss-map').setView([0, 0], 1)
+// Add a tile layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+// Create an icon for the ISS
 let IssIcon = L.icon({
     iconUrl: 'international-space-station.png',
     iconSize: [50, 50],
     iconAnchor: [25, 25]
 })
 
+// Variable to store the marker
 let issMarker
+// Variables for the update interval and the maximum failed attempts
 let update = 10000
 let maxFailedAttempts = 3
 
+// Call the function to update the ISS location
 updateISS(maxFailedAttempts)
 
 function updateISS(attempts) {
-    
+
     // If the attempts are less than or equal to 0, alert the user and return
     if (attempts <= 0) {
         alert("Failed to contact ISS server after several attempts")
         return
     }
 
+    // Fetch the data from the API
     fetch(url)
         .then( res => res.json() )
         .then( issData => { 
             console.log(issData) 
+            // Get the latitude and longitude from the data and display it
             let lat = issData.latitude
             let long = issData.longitude
             issLat.innerHTML = lat 
             issLong.innerHTML = long
 
+            // Get the current date and time and display it
             let now = Date()
             timeIssLocationFetched.innerHTML = `This data was fetched at ${now}`
 
@@ -56,7 +67,7 @@ function updateISS(attempts) {
         // Finally runs weather the fetch is successful or not
         .finally( () => {
             // Call the function again after 10 seconds
-            // So regardless of the outcome of the fetch, it will be called again
+            // So regardless of the outcome of the fetch, it will be called again as long as the attempts are greater than 0
             setTimeout(updateISS, update, attempts)
         })
 }
