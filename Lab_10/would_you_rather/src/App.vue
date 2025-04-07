501f1e76faa4d392bbfb6acca8ba47d056e71aff
mmcel <mm69@yahoo.com>
2025-04-07 14:49:06 -0500

    Working on lab_10 part 1

--- Lab_10/would_you_rather/src/App.vue
+++ Lab_10/would_you_rather/src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
+import { ref } from 'vue'
+
+const question = ref('Go to a heavy metal concert or a rap concert?')
+const answer1 = ref('Heavy Metal')
+const answer2 = ref('Rap')
+const userSelection = ref('')
+
+function updateUserSelection(userAnswer) {
+  userSelection.value = `Thanks for answering! You chose: ${userAnswer}`
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+  <h1>Would You Rather...</h1>
+
+  <WouldYouRather 
+    v-bind:question="question"
+    v-bind:answer1="answer1"
+    v-bind:answer2="answer2"
+    v-on:answer-selected="updateUserSelection">
+  </WouldYouRather>
+
+
+  <p>{{ userSelection }}</p>
+
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ Lab_10/would_you_rather/src/components/WouldYouRather.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+const emit = defineEmits([
+  'answer-selected'
+])
+
+const userAnswer = ref('')
+
+function choiceMade() {
+  emit('answer-selected', userAnswer.value)
+}
+
+</script>
+
+<template>
+ 
+  <h2>Make your choice!</h2>
+
+  {{ question }}
+
+  <input type="radio" id="answer1" v-model="userAnswer" v-bind:value="answer1" v-on:change="choiceMade">
+  <label for="answer1">{{ answer1 }}</label>
+  <input type="radio" id="answer2" v-model="userAnswer" v-bind:value="answer2" v-on:change="choiceMade">
+  <label for="answer2">{{ answer2 }}</label>
+
+</template>
+
+<style scoped>
+
+</style>
