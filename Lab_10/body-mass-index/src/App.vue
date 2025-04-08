eb2e565b996839149b059d8962adc910d455a20c
mmcel <mm69@yahoo.com>
2025-04-08 17:11:25 -0500

    Working on BodyMassIndex everything is working at the moment...

--- Lab_10/body-mass-index/src/App.vue
+++ Lab_10/body-mass-index/src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
 import BodyMassIndex from './components/BodyMassIndex.vue'
+import { ref } from 'vue'
+
+const bmi = ref(0)
+
+function calculateBMI([weight, height]) {
+        bmi.value = (weight / (height * height)).toFixed(2)
+}
+
 </script>
 
 <template>
 
     <h1>Body Mass Index Calculator</h1>
 
-    <BodyMassIndex></BodyMassIndex>
+    <BodyMassIndex @stats-entered="calculateBMI"></BodyMassIndex>
 
+    <p>{{ bmi }}</p>
   
 </template>
 
--- Lab_10/body-mass-index/src/components/BodyMassIndex.vue
+++ Lab_10/body-mass-index/src/components/BodyMassIndex.vue
@@ -1,21 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-defineProps({
 
-})
+
+const emit = defineEmits([
+    'stats-entered'
+])
+
+
+const height = ref(0)
+const weight = ref(0)
+
+
 
 </script>
 
 <template>
 
     <h2>Enter your height and weight</h2>
+
     <div id="inputs">
-    <p><input type ="number" id="heightInput" v-model="height"><label for="heightInput">Height</label></p>
+
+        <p><input type ="number" id="heightInput" v-model="height"><label for="heightInput">Height</label></p>
     
-    <p><input type ="number" id="weightInput" v-model="weight"><label for="weightInput">Weight</label></p>
+        <p><input type ="number" id="weightInput" v-model="weight"><label for="weightInput">Weight</label></p>
+
     </div>
-    <button id="calculateButton">Calculate</button>
+
+    <button id="calculateButton" @click="emit('stats-entered',[height, weight])">Calculate</button>
   
 </template>
 
