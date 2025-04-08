b01f219d3a493db0cdb9058ff2c1af0c280239bf
mmcel <mm69@yahoo.com>
2025-04-07 22:58:11 -0500

    Working on BodyMassIndex

--- Lab_10/body-mass-index/src/App.vue
+++ Lab_10/body-mass-index/src/App.vue
@@ -3,6 +3,11 @@ import BodyMassIndex from './components/BodyMassIndex.vue'
 </script>
 
 <template>
+
+    <h1>Body Mass Index Calculator</h1>
+
+    <BodyMassIndex></BodyMassIndex>
+
   
 </template>
 
--- Lab_10/body-mass-index/src/components/BodyMassIndex.vue
+++ Lab_10/body-mass-index/src/components/BodyMassIndex.vue
@@ -2,15 +2,38 @@
 import { ref } from 'vue'
 
 defineProps({
-  
+
 })
 
 </script>
 
 <template>
+
+    <h2>Enter your height and weight</h2>
+    <div id="inputs">
+    <p><input type ="number" id="heightInput" v-model="height"><label for="heightInput">Height</label></p>
+    
+    <p><input type ="number" id="weightInput" v-model="weight"><label for="weightInput">Weight</label></p>
+    </div>
+    <button id="calculateButton">Calculate</button>
   
 </template>
 
 <style scoped>
-
+#heightInput, #weightInput {
+    width: 100px;
+}
+#calculateButton {
+    margin-top: 10px;
+    padding: 5px 10px;
+    background-color: #4CAF50;
+    color: white;
+}
+#inputs {
+    background-color: blue;
+    color: white;
+    padding: 10px;
+    border-radius: 5px;
+    align-items: center;
+}
 </style>
