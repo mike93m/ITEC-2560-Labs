bcf94770669989bb9820ac1c886ede68523c9d2f
mmcel <mm69@yahoo.com>
2025-04-27 16:56:42 -0500

    Finished the spowerpoint part of lab_12

---
+++ Lab_12/wyr/server.js
@@ -0,0 +1,16 @@
+// Use express
+const express = require('express')
+
+// Import the routes from index.js
+const routes = require('./routes/index')
+
+// Create an express app
+const app = express()
+
+// Make the routes available in the app
+app.use('/', routes)
+
+// Start the server running
+let server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${server.address().port}`)
+})
