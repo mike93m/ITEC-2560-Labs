284c3706ba790fc756083e8c38cca84dea981d87
mmcel <mm69@yahoo.com>
2025-05-04 21:25:55 -0500

    Added some comments to Lab 12. I didnt comment anything from the student_feedback_form.hbs because its pretty obvious whats going on in that file.

--- Lab_12/student_sign_in/routes/index.js
+++ Lab_12/student_sign_in/routes/index.js
@@ -1,19 +1,23 @@
+// Use express router to handle the routes for the application
+// Import the express module and create a router object
 const express = require('express')
 const router = express.Router()
 
+// Get the home page and render the view
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Feedback app', author: 'Mike M' })
 })
 
+// Get the feedback form page and render the view
 router.get('/feedback-form', function(req, res, next) {
     res.render('student_feedback_form')
 })
 
+// Post the feedback form data to the server
 router.post('/submit-feedback', function(req, res, next) {
     // Get the form data from the URL query
     const formData = req.body
-    // TODO save the form data to the database
-
+    // Display the form data the user has submitted
     res.render('thank_you', {
         name: formData.name,
         email: formData.email,
@@ -21,5 +25,5 @@ router.post('/submit-feedback', function(req, res, next) {
         currentStudent: formData['student-status'],
     })
 })
-    
+// Export the router object to be used in other modules
 module.exports = router
