852ea59bd5ec838191cb91466476b66f1b389a0f
mmcel <mm69@yahoo.com>
2025-05-06 22:21:43 -0500

    Working on lab 14

--- Lab_14/student-sign-in-az/models/student.js
+++ Lab_14/student-sign-in-az/models/student.js
@@ -1,17 +1,25 @@
+const { all } = require("../routes/api")
+
 module.exports = (sequelize, DataTypes) => {
     const student = sequelize.define('Student', {
         name: {
             type: DataTypes.STRING,
+            allowNull: false
         },
         starID: {
             type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
         },
         present: {
             type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
         }
     })
     student.sync({force: false}).then( () => {
         console.log('Synced student table')
     })
+    
     return student
 }
--- Lab_14/student-sign-in-az/server.js
+++ Lab_14/student-sign-in-az/server.js
@@ -11,6 +11,15 @@ app.use(express.json())
 // Use api routes for all requests to /api
 app.use('/api', apiRoutes)
 
+app.use(function(req,res,next) {
+    res.status(404).send('Not found')
+})
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack) // Log the error stack to the console for server-side debugging
+    res.status(500).send('Server error') // Send a 500 error response to the client
+})
+
 // Start the server on port 3000
 const server = app.listen(process.env.PORT || 3000, function() {
     console.log('Server is running on port ', server.address().port)
